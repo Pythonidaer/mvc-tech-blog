0f5fabb21a94230bff443f1cae8cb7934be6ceae
Pythonidaer <pythonidaer@gmail.com>
2021-03-28 20:00:50 -0400

    Connected basic GET route to handlebars page. Am going to work on models after dinner

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
     try {
-        await res.json('Hello');
+        await res.render('homepage');
     } catch (err) {
         res.status(500).json(err);
       }
