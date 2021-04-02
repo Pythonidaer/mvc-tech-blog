15901abdc8d6798cecd9cddf3bfa900e7e8ae1d9
Pythonidaer <pythonidaer@gmail.com>
2021-04-02 19:44:39 -0400

    debugged one post with tutor

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,13 @@ let sequelize;
 
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
+    // console.log('jaws');
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
         port: 3306
     });
+    // console.log('local');
 }
 module.exports = sequelize;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -12,31 +12,37 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     Post.findAll({
-            attributes: [
-                'id',
-                'title',
-                'content',
-                'created_at'
-            ],
-            include: [{
-                    model: Comment,
-                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                    include: {
-                        model: User,
-                        attributes: ['username']
-                    }
-                },
-                {
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [{
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
                     model: User,
                     attributes: ['username']
                 }
-            ]
-        })
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
         .then(dbPostData => {
+            // console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+            // console.log(dbPostData)
+            // console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
             const posts = dbPostData.map(post => post.get({ plain: true }));
             // res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+            console.log('------------------------------------------');
             console.log(posts);
-            res.render('homepage', { posts });
+            console.log('------------------------------------------');
+            // giving an obj w/ a prop called posts that is and array of obj(post objects)
+            res.render('homepage', { post: posts[0] });
         })
         .catch(err => {
             console.log(err);
