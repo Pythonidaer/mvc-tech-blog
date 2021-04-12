52696c699dea5dfb171dab35038590fe88eb1bea
Pythonidaer <pythonidaer@gmail.com>
2021-04-11 22:10:29 -0400

    The styling still needs a lot of work, but the routes are really fun. I still wish the bootcamp decided to choose either jQuery or JavaScript and stopped making learning fetches confusing, but at any rate at least now I understand what is going on with the post data.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -65,8 +65,53 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+// same as our login page, except signup counts as a login
 router.get('/signup', (req, res) => {
     res.render('signup');
+});
+
+// when a user clicks on a post on the home page, view it
+// eventually this will be where a user can add comments
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'content',
+                'title',
+                'created_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            const post = dbPostData.get({ plain: true });
+            console.log(post);
+            res.render('single-post', { post, loggedIn: req.session.loggedIn });
+
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
