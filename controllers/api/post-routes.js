3c2c62c16b51b5a4aac4762fe442b73a31ab4af9
Pythonidaer <pythonidaer@gmail.com>
2021-04-11 20:26:48 -0400

    Accomplished user login and post creation. Still working on making website responsive, but first half to figure  first have to figure out how to edit and delete posts.

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,135 @@
+// import the express router object
+const router = require('express').Router();
+// include our required objects for these routes
+const { Post, User, Comment } = require('../../models');
+// import our sequelize connection to the database
+const sequelize = require('../../config/connection');
+// import our loggedIn authenticator since users can't post unless in
+const withAuth = require('../../utils/auth');
+
+// when a post is added, find all it's content and post it in reverse
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+            attributes: ['id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            order: [
+                ['created_at', 'DESC']
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        })
+        .then(dbPostData => res.json(dbPostData.reverse()))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// when a user clicks on a specific post, return all that data
+router.get('/:id', (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id',
+                'content',
+                'title',
+                'created_at'
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// After user submits a new post, connect user session then get above
+router.post('/', withAuth, (req, res) => {
+    Post.create({
+            title: req.body.title,
+            content: req.body.content,
+            user_id: req.session.user_id
+        })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// when user clicks 'update' button, replace post-id data with new
+router.put('/:id', withAuth, (req, res) => {
+    Post.update({
+            title: req.body.title,
+            content: req.body.content
+        }, {
+            where: {
+                id: req.params.id
+            }
+        }).then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// when user clicks 'delete' button, remove record from database entirely
+router.delete('/:id', withAuth, (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -29,8 +29,8 @@ router.get('/', withAuth, (req, res) => {
         })
         .then(dbPostData => {
             const posts = dbPostData.map(post => post.get({ plain: true }));
-            // res.render('dashboard', { posts, loggedIn: true });
-            res.render('dashboard');
+            res.render('dashboard', { posts, loggedIn: true });
+            // res.render('dashboard');
         })
         .catch(err => {
             console.log(err);
@@ -38,7 +38,52 @@ router.get('/', withAuth, (req, res) => {
         });
 });
 
+// when a user clicks to edit a specific id, make sure hey are logged in
+router.get('/edit/:id', withAuth, (req, res) => {
+    // if user is logged in, find that post by id
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        })
+        // if there is no post with that data found by id, post error
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
 
+            // otherwise return object rendered to edit-post page
+            const post = dbPostData.get({ plain: true });
+            res.render('edit-post', { post, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
+// when a user clicks /new to add a post, render that page
+router.get('/new', (req, res) => {
+    res.render('add-post');
+});
 
 module.exports = router;
